47df35585a4d3f0f5831e404d22e1a4ca6434500
Yixiao Hu <yhu32@sheffield.ac.uk>
2016-12-10 19:34:50 +0000

    first commit

---
+++ assignment.rb
@@ -0,0 +1,8 @@
+# Created with com1001---ruby-assignment.
+# User: yhu
+# Date: 2016-12-10
+# Time: 07:30 PM
+# To change this template use Tools | Templates.
+class /assignment
+    //put your code here
+end
